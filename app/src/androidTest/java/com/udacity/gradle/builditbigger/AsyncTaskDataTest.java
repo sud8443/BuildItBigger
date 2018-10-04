2e4028081c2e33b337f43da30c6a109544f5b64a
Sudhanshu Vohra <sudhanshuvohra8443@gmail.com>
2018-10-04 05:33:09 +0530

    Added a test to the Async Task to check that it doesn't returns non-empty string

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskDataTest.java
@@ -0,0 +1,35 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Pair;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static junit.framework.Assert.assertTrue;
+
+
+@RunWith(AndroidJUnit4.class)
+public class AsyncTaskDataTest {
+
+    private Context context;
+
+    @Before
+    public void setup() {
+        context = InstrumentationRegistry.getContext();
+    }
+
+    @Test
+    public void checkNonEmptyReturnValue(){
+        EndPointAsyncTask asyncTask = new EndPointAsyncTask();
+        asyncTask.execute(new android.support.v4.util.Pair<Context, String>(context, "Test String"));
+        asyncTask.setJokeResultListner(new EndPointAsyncTask.JokeResultListener() {
+            @Override
+            public void gotJokeFromApi(String joke) {
+                assertTrue(joke, joke != null && joke.length() > 0);
+            }
+        });
+    }
+}
